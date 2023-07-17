2062c6c707e40131126fee79990a0e26e5db8e1c
Pothuraj <you@example.com>
2023-07-17 14:17:33 -0400

    refactoring

--- index.js
+++ index.js
@@ -26,24 +26,4 @@ function start( ){
        ${port}!`))
 }
 
-module.exports = start;
-// app.get('/', (req, res) => {
- 
-//  var dataToSend;
-//  // spawn new child process to call the python script
-//  const python = spawn('python', ['streamer_model.py']);
-//  // collect data from script
-//  python.stdout.on('data', function (data) {
-//   console.log('Pipe data from python script ...');
-//   dataToSend = data.toString();
-//  });
-//  // in close event we are sure that the stream from child process is closed
-//  python.on('close', (code) => {
-//  console.log(`child process close all stdio with code ${code}`);
-//  // send data to the browser
-//  res.send(dataToSend)
-//  });
- 
-// })
-// app.listen(port, () => console.log(`Example app listening on port 
-// ${port}!`))
+module.exports = start;   // server as a module
